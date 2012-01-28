a5dbf796d4114166993dac031dd6447ec071686b
Prodis <prodis@gmail.com>
2012-01-28 19:49:13 -0200

    Init Rastreador class.

--- lib/correios/sro/rastreador.rb
+++ lib/correios/sro/rastreador.rb
@@ -2,6 +2,21 @@
 module Correios
   module SRO
     class Rastreador
+      attr_accessor :usuario, :senha
+      attr_accessor :tipo, :resultado
+
+      DEFAULT_OPTIONS = { :tipo => :lista, :resultado => :ultimo }
+
+      def initialize(options = {})
+        DEFAULT_OPTIONS.merge(options).each do |attr, value|
+          self.send("#{attr}=", value)
+        end
+
+        yield self if block_given?
+      end
+
+      def consultar(*objetos)
+      end
     end
   end
 end
--- spec/correios/sro/rastreador_spec.rb
+++ spec/correios/sro/rastreador_spec.rb
@@ -3,10 +3,38 @@ require 'spec_helper'
 
 describe Correios::SRO::Rastreador do
   describe ".new" do
-    it "creates new tracker" do
-      sro = Correios::SRO::Rastreador.new
-      sro.should_not be_nil
+    context "creates with default value of" do
+      before(:each) { @sro = Correios::SRO::Rastreador.new }
+
+      { :tipo => :lista, :resultado => :ultimo }.each do |attr, value|
+        it attr do
+          @sro.send(attr).should == value
+        end
+      end
     end
+
+    { :usuario => "USUARIO",
+      :senha => "SENHA",
+      :tipo => :intervalo,
+      :resultado => :todos
+    }.each do |attr, value|
+      context "when #{attr} is supplied" do
+        it "sets #{attr}" do
+          sro = Correios::SRO::Rastreador.new(attr => value)
+          sro.send(attr).should == value
+        end
+      end
+
+      context "when #{attr} is supplied in a block" do
+        it "sets #{attr}" do
+          sro = Correios::SRO::Rastreador.new { |f| f.send("#{attr}=", value) }
+          sro.send(attr).should == value
+        end
+      end
+    end
+  end
+
+  describe "#consultar" do
   end
 end
 
