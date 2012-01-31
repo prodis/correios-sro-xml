bd4ee69c8d4b1b5408d5c830df6b49d4040bc8be
Prodis <prodis@gmail.com>
2012-01-31 07:45:51 -0200

    Spec for WebService#request.

--- spec/correios/sro/web_service_spec.rb
+++ spec/correios/sro/web_service_spec.rb
@@ -2,4 +2,13 @@
 require 'spec_helper'
 
 describe Correios::SRO::WebService do
+  describe "#request" do
+    let(:sro) { Correios::SRO::Rastreador.new }
+    let(:web_service) { Correios::SRO::WebService.new }
+
+    it "returns XML response" do
+      fake_request_for("<xml><fake></fake>")
+      web_service.request(sro).should == "<xml><fake></fake>"
+    end
+  end
 end
