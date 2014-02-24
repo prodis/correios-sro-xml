4686072d01be4bac4bd30bc99e5468e5c4d7c198
Prodis <prodis@gmail.com>
2014-02-24 14:03:16 -0300

    Refactory Event specs.

--- spec/correios/sro/event_spec.rb
+++ spec/correios/sro/event_spec.rb
@@ -1,14 +1,11 @@
-# encoding: UTF-8
 require 'spec_helper'
 
 describe Correios::SRO::Event do
-  let(:event) { Correios::SRO::Event.new }
-
   [:receiver, :document, :comment].each do |method|
     describe "##{method}=" do
       it "strips string" do
-        event.send("#{method}=", "   Texto.   ")
-        event.send(method).should == "Texto."
+        subject.send("#{method}=", "   Texto.   ")
+        expect(subject.send(method)).to eql "Texto."
       end
     end
   end
