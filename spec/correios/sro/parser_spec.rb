8b5d8d74d47ecbc92311e688f3a1e9e2dcace2a1
Prodis <prodis@gmail.com>
2014-02-24 21:05:05 -0300

    Refactory Parser specs.

--- spec/correios/sro/parser_spec.rb
+++ spec/correios/sro/parser_spec.rb
@@ -1,55 +1,53 @@
-# encoding: UTF-8
 require 'spec_helper'
 
 describe Correios::SRO::Parser do
   describe "#objects" do
-    let(:xml) { body_for :success_response_many_objects }
-    let(:parser) { Correios::SRO::Parser.new }
+    let(:xml) { Fixture.load :sro_many_objects }
 
     it "encodes from ISO-8859-1 to UTF-8" do
       xml.should_receive(:backward_encode).with("UTF-8", "ISO-8859-1").and_return(xml)
-      parser.objects(xml)
+      subject.objects(xml)
     end
 
     ["SI047624825BR", "SX104110463BR"].each do |number|
       it "returns object number" do
-        objects = parser.objects(xml)
-        objects[number].number.should == number
+        objects = subject.objects(xml)
+        expect(objects[number].number).to eql number
       end
     end
 
     context "returns event" do
-      before(:each) { @objects = parser.objects(xml) }
+      before { @objects = subject.objects(xml) }
 
       { "SI047624825BR" => {
-          :type => "BDI",
-          :status => "01",
-          :date => "26/12/2011",
-          :hour => "15:22",
-          :description => "Entregue",
-          :receiver => "",
-          :document => "",
-          :comment => "?",
-          :place => "AC CENTRAL DE SAO PAULO",
-          :code => "01009972",
-          :city => "SAO PAULO",
-          :state => "SP",
-          :sto => "00024419"
+          type: "BDI",
+          status: "01",
+          date: "26/12/2011",
+          hour: "15:22",
+          description: "Entregue",
+          receiver: "",
+          document: "",
+          comment: "?",
+          place: "AC CENTRAL DE SAO PAULO",
+          code: "01009972",
+          city: "SAO PAULO",
+          state: "SP",
+          sto: "00024419"
         },
         "SX104110463BR" => {
-          :type => "BDE",
-          :status => "01",
-          :date => "08/12/2011",
-          :hour => "09:30",
-          :description => "Entregue",
-          :receiver => "",
-          :document => "",
-          :comment => "",
-          :place => "CEE JUNDIAI",
-          :code => "13211970",
-          :city => "JUNDIAI",
-          :state => "SP",
-          :sto => "74654209"
+          type: "BDE",
+          status: "01",
+          date: "08/12/2011",
+          hour: "09:30",
+          description: "Entregue",
+          receiver: "",
+          document: "",
+          comment: "",
+          place: "CEE JUNDIAI",
+          code: "13211970",
+          city: "JUNDIAI",
+          state: "SP",
+          sto: "74654209"
         },
       }.each do |number, first_event|
         first_event.each do |attr, value|
@@ -62,27 +60,27 @@ describe Correios::SRO::Parser do
     end
 
     context "returns destination" do
-      before(:each) { @objects = parser.objects(xml) }
+      before { @objects = subject.objects(xml) }
 
       { "SI047624825BR" => {
-          :place => "CTE VILA MARIA",
-          :code => "02170975",
-          :city => "SAO PAULO",
-          :neighborhood => "PQ NOVO MUNDO",
-          :state => "SP"
+          place: "CTE VILA MARIA",
+          code: "02170975",
+          city: "SAO PAULO",
+          neighborhood: "PQ NOVO MUNDO",
+          state: "SP"
         },
         "SX104110463BR" => {
-          :place => "CTE CAMPINAS",
-          :code => "13050971",
-          :city => "VALINHOS",
-          :neighborhood => "MACUCO",
-          :state => "SP"
+          place: "CTE CAMPINAS",
+          code: "13050971",
+          city: "VALINHOS",
+          neighborhood: "MACUCO",
+          state: "SP"
         },
       }.each do |number, destinations|
         destinations.each do |attr, value|
           it attr do
             destination = @objects[number].events[3].destination
-            destination.send(attr).should == value
+            expect(destination.send(attr)).to eql value
           end
         end
       end
